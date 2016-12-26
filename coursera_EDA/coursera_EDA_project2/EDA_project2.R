ed778d52d9876e7fd6b3857fe3353d3ca5e2626b
LTaeHoon <lth1011@gmail.com>
2016-12-26 18:24:06 +0900

    answer code  for question 1,2,3

--- coursera_EDA/coursera_EDA_project2/EDA_project2.R
+++ coursera_EDA/coursera_EDA_project2/EDA_project2.R
@@ -1,10 +1,35 @@
+if(!exist(NEI)){
 NEI <- readRDS("data//summarySCC_PM25.rds")
+}
+if(!exist(SCC)){
 SCC <- readRDS("data//Source_Classification_Code.rds")
-
+}
 # 1. Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 
-head(NEI)
-head(SCC)
+data1 <- aggregate(NEI$Emissions,by=list(NEI$year),sum)
+colnames(data1) <- c("year","Emissions")
+data1$Emissions <- round(data1$Emissions/1000,2)
+
+png('data/plot1.png')
+barplot(data1$Emissions,names.arg=data1$year,xlab="year",ylab="Emissions(kilotons)",main="Total Emissions of PM2.5")
+dev.off()
+
+
+# 2. Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? Use the base plotting system to make a plot answering this question.
+# subset by fips=='24510'
+str(NEI)
+Maryland_data <- subset(NEI,fips=="24510",c(fips,Emissions,year))
+data2 <- aggregate(Maryland_data$Emissions,by=list(Maryland_data$year),sum)
+colnames(data2) <- c("year","Emissions")
+head(data2)
+png('data/plot2.png')
+barplot(data2$Emissions,names.arg = data2$year,xlab="year",ylab="Emissions",main="Total emissions of PM2.5 in Maryland")
+dev.off()
+
+# 3. Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? Which have seen increases in emissions from 1999–2008? Use the ggplot2 plotting system to make a plot answer this question.
+
+str(NEI)
+str(SCC)
 
 fips1999 <- subset(NEI,year==1999,c(fips,Emissions))
 sum(is.na(fips1999$Emissions))#0 -> na값이 없음
@@ -42,7 +67,7 @@ segments(rep(2005,nrow(mrg)),mrg[,4],rep(2008,nrow(mrg)),mrg[,5])
 
 ?segments
 
-# 2. Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? Use the base plotting system to make a plot answering this question.
+
 # 3. Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? Which have seen increases in emissions from 1999–2008? Use the ggplot2 plotting system to make a plot answer this question.
 # 4. Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008?
 # 5. How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore City?
